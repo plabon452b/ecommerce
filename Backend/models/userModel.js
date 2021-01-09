7f888ddcbe3b22da1062677b0dd245c31f8f41aa
plabonCSEDU24 <sakibhasan.24csedu.062@gmail.com>
2021-01-09 17:07:39 +0600

    final project

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,30 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     role: {
-        type: Number,
-        default: 0
+      type: Number, //1 for admin 0 for user
+      default: 0,
     },
     cart: {
-        type: Array,
-        default: []
-    }
-}, {
-    timestamps: true
-})
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Users', userSchema)
+module.exports = mongoose.model("Users", userSchema);
